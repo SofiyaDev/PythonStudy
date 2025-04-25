34674a390fb5a6572cdd81ac43c06102236be3c5
SofiyaDev <sofi2010fo14@gmail.com>
2025-04-25 21:18:02 +0300

    new changes 2

---
+++ indexes_and_slices.py
@@ -0,0 +1,11 @@
+lis = [3, 6, "gota", False, ("Solomka", 5), 4.6, [1, 2, 3]]
+newList = lis[::-1]
+print(newList)
+a = "Vse?"
+
+katya = [1, 2, 3, 4, 5]
+solomka = katya[::-1]
+katya.remove(1)
+katya.remove(3)
+katya.append(13)
+print(katya, solomka) #immutable collections
