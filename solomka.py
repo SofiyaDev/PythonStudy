7ef5411ce395362453ac183d1ba6d7be568189e1
SofiyaDev <sofi2010fo14@gmail.com>
2025-04-25 21:04:29 +0300

    new file added

---
+++ solomka.py
@@ -0,0 +1,7 @@
+list = [0, 1, 4.5]  # ctrl + d - duplicate line
+list = [0, 1, 4.5]  # ctrl + d - duplicate line
+#
+# a = 1 # ctrl + / comment lines
+# a = -435
+# print(a)
+# ctrl + alt + l - format file
