cc342ab24f9469cfdaad532accc01c862650539b
SofiyaDev <sofi2010fo14@gmail.com>
2025-05-07 16:21:06 +0300

    finish first py

--- 16_module.py
+++ 16_module.py
@@ -1,11 +1,10 @@
-
+import cowsay
 import my_module
 import time
 import datetime
 import sys
 import os, platform
 from math import sqrt, ceil as c
-
 
 print(c(sqrt(144)))
 
@@ -19,4 +18,5 @@ print(platform.system())
 my_module.hi()
 print(my_module.name)
 
+cowsay.cow("FREDYA")
 
